e450bd7b34f35435498480568917858563321985
dewoolkaridhish4 <65116701+dewoolkaridhish4@users.noreply.github.com>
2020-07-03 16:32:08 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,21 @@
+class Box {
+  constructor(x, y, width, height) {
+    super(x,y,50,50);
+    this.image = loadImage("Sprites/Ball.png");
+    this.visibility=255;
+
+  }
+  display(){
+    if(this.body.speed<3){
+      super.display();
+    }
+    else{
+      World.remove(world,this.body);
+      push();
+      this.visibility=this.visibility-5;
+      tint(255,this.visibility);
+      image(this.image,this.body.position.x,this.body.position.y,50,50);
+      pop();
+    }
+  }
+};
